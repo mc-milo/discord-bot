b86f97ca929842650de4c3bda22ce615421e245d
= <=>
2023-03-12 19:58:12 +0200

    added import so slash commands can be used in cogs

--- src/cogs/embed.py
+++ src/cogs/embed.py
@@ -1,4 +1,5 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
 
 class Emb(commands.Cog):
@@ -25,7 +26,21 @@ class Emb(commands.Cog):
 
         await ctx.send(embed = embed_message)
 
+    @app_commands.command()
+    async def embed(self, interaction: discord.Interaction):
+        embed_message = discord.Embed(
+                                    title="Title of embed", 
+                                    description="Description of embed", 
+                                    color=discord.Color.random() # interaction.user.color as well
+                                    ) 
+        
+        embed_message.set_author(name=f"Requested by {interaction.user.name}", icon_url=interaction.user.avatar)
+        embed_message.add_field(name="field name", value="field value", inline=False)
+        embed_message.set_thumbnail(url=interaction.guild.icon)
+        embed_message.set_image(url=interaction.guild.icon)
+        embed_message.set_footer(text="this is the footer", icon_url=interaction.user.avatar)
+
+        await interaction.response.send_message(embed=embed_message, ephemeral=True)
     
 async def setup(client):
     await client.add_cog(Emb(client))
-    
