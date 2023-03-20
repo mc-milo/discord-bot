4fbdbc429c6c71c4a9f8a9197dd13a791838528e
= <=>
2023-03-20 12:35:22 +0200

    template update

--- templates/Cog_Template.py
+++ templates/Cog_Template.py
@@ -3,12 +3,12 @@ from discord import app_commands
 from discord.ext import commands
 
 class MyCog(commands.Cog):
-    def __init__(self, client):
+    def __init__(self, client: discord.client.Client):
         self.client = client
 
     @commands.Cog.listener()
     async def on_ready(self):
         print("MyCog is online")
-    
+
 async def setup(client):
     await client.add_cog(MyCog(client))
